8347d509fa8a00a1325ecbb50b7ad7bbf8b889c1
RafaVergara95 <rafa95@live.cl>
2022-04-13 18:14:09 -0400

    patrones de disenio 14

---
+++ pd14/dao/IConexion.java
@@ -0,0 +1,6 @@
+package dao;
+
+public interface IConexion {
+
+	void conectar();
+}
---
+++ pd14/dao/PersonaDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+import model.Persona;
+
+public interface PersonaDAO extends CRUD<Persona>{
+
+	void mostrarNombre();
+	void setConexion(IConexion conexion);
+	
+}
